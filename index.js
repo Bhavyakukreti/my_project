39075e1d5325ad11fb4a470c48353b7c3dfad47a
Bhavyakukreti <139851637+Bhavyakukreti@users.noreply.github.com>
2023-10-24 21:36:30 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+var a=document.querySelector("#hi");
+a.addEventListener("click",function(){
+    
+    document.querySelector("#momo").innerHTML="Afifa Sampa The standard chunk of" + "<br/>" + " Lorem Ipsum used since the 1500s is " + "<br/>" + "reproduced below for those interested. " + "<br/>" + "Sections Bonorum et Malorum original.";
+    document.querySelector("#burger").innerHTML="AFIFA SAMPA - COO, AMERIMAR ENTERPRISES, INC."
+
+})
+var a=document.querySelector("#hell");
+a.addEventListener("click",function(){
+    document.querySelector("#momo").innerHTML="John The standard chunk of Lorem" + "<br/>" + "Ipsum used since the 1500s is" + "<br/>" + "reproduced below for those interested." + "<br/>" + "Sections Bonorum et Malorum original.";
+    document.querySelector("#burger").innerHTML="JOHN NIRA - COO, AMERIMAR ENTERPRISES, INC."
+})
+    
+var a=document.querySelector("#bye");
+a.addEventListener("click",function(){
+    document.querySelector("#momo").innerHTML="Jone The standard chunk of Lorem" + "<br/>" + " Ipsum used since the 1500s is" + "<br/>" + " reproduced below for those interested." + "<br/>" + " Sections Bonorum et Malorum original.";
+    document.querySelector("#burger").innerHTML="JONE JANE - COO, AMERIMAR ENTERPRISES, INC."
+})
+    
+var a=document.querySelector("#sye");
+a.addEventListener("click",function(){
+    document.querySelector("#momo").innerHTML="Janen The standard chunk of Lorem Ipsum" + "<br/>" + " used since the 1500s is " + "<br/>" + "reproduced below for those interested." + "<br/>" + " Sections Bonorum et Malorum original.";
+    document.querySelector("#burger").innerHTML="JANEN ASRAFY - COO, AMERIMAR ENTERPRISES, INC."
+})
+    
+var a=document.querySelector("#gye");
+a.addEventListener("click",function(){
+    document.querySelector("#momo").innerHTML="Irin The standard chunk of Lorem" + "<br/>" + " Ipsum used since the 1500s is" + "<br/>" + " reproduced below for those interested. " + "<br/>" + "Sections Bonorum et Malorum original.";
+    document.querySelector("#burger").innerHTML="FATIMA ASRAFY - COO, AMERIMAR ENTERPRISES, INC."
+})
+    
+var a=document.querySelector("#mye");
+a.addEventListener("click",function(){
+    document.querySelector("#momo").innerHTML="Mohima The standard chunk of Lorem " + "<br/>" + " Ipsum used since the 1500s is reproduced" + "<br/>" + " below for those interested. Sections" + "<br/>" + " Bonorum et Malorum original.";
+    document.querySelector("#burger").innerHTML="IRIN ASRAFY - COO, AMERIMAR ENTERPRISES, INC."
+})
+    
+var a=document.querySelector("#lye");
+a.addEventListener("click",function(){
+    document.querySelector("#momo").innerHTML="Shipa The standard chunk of Lorem" + "<br/>" + " Ipsum used since the 1500s is " + "<br/>" + "reproduced below for those interested." + "<br/>" + " Sections Bonorum et Malorum original.";
+    document.querySelector("#burger").innerHTML="MOHIMA ASRAFY - COO, AMERIMAR ENTERPRISES, INC."
+})
+    
+var a=document.querySelector("#xye");
+a.addEventListener("click",function(){
+    document.querySelector("#momo").innerHTML="John The standard chunk of Lorem" + "<br/>" + " Ipsum used since the 1500s is" + "<br/>" + " reproduced below for those interested. " + "<br/>" + "Sections Bonorum et Malorum original.";
+    document.querySelector("#burger").innerHTML="SHIPA - COO, AMERIMAR ENTERPRISES, INC."
+})
+    
+
+    
